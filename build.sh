e7932004bdf3cde3cf47766683e4c584b4f5f74c
Michael John <michael.john@gmx.at>
2023-08-16 13:48:31 +0200

    added ISO signing

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ b2sum "astrolinux-$(date +%Y.%m.01)-x86_64.iso" >> b2sums.txt
 md5sum "astrolinux-$(date +%Y.%m.01)-x86_64.iso" >> md5sums.txt
 sha1sum "astrolinux-$(date +%Y.%m.01)-x86_64.iso" >> sha1sums.txt
 sha256sum "astrolinux-$(date +%Y.%m.01)-x86_64.iso" >> sha256sums.txt
+# rm "astrolinux-$(date +%Y.%m.01)-x86_64.iso.asc"
+gpg --y --detach-sign -a "astrolinux-$(date +%Y.%m.01)-x86_64.iso"
+gpg --y --export --armor --output astrolinux-public-key.asc "Michael John <amstelchen@gmx.at>"
